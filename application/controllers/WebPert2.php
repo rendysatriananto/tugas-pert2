c0de943bb4b257759857e023cc7a9ab65e9e06d6
rendysatriananto <rendysatriananto@gmail.com>
2021-09-28 01:08:58 +0700

    Tugas Pert 2 Web II

---
+++ application/controllers/WebPert2.php
@@ -0,0 +1,13 @@
+<?php
+class WebPert2 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Rendy Satriananto
+             Saya tinggal di Klanen Dagaran
+             Olah raga yang saya sukai adalah 
+             Gym";
+
+    }
+}
